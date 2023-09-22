660dfb63627bdb2f4445647a21b7103420d05742
SamzyDev <144393153+sstock2005@users.noreply.github.com>
2023-09-22 08:03:59 -0500

    oops
    - Tried to fix getpos

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import cv2
-from frc_apriltags import Detector
+from frc_apriltags import Detector, NetworkCommunications, USBCamera
 import numpy as np
-
+from pathlib import Path
 # Initialize the AprilTag detector
 detector = Detector(6)
+
+# Get the directory path
+dirPath = Path(__file__).absolute().parent.__str__()
 
 # Add way to send data to network table
 
 # Open the USB camera
 camera = cv2.VideoCapture(0)
 
+# Calibrate Camera
+calicamera = USBCamera(0, None, (1280,720), 15, True, dirPath)
+
+net = NetworkCommunications()
 while True:
     # Read a frame from the camera
     ret, frame = camera.read()
@@ -20,8 +27,8 @@ while True:
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect AprilTags in the frame
-    detections = detector.detector.detect(gray_frame)
-
+    #detections = detector.detector.detect(gray_frame)
+    detections = detector.detectTags(gray_frame, calicamera.getMatrix(), 3)
     # Filter detections based on size and pose
     valid_detections = []
     for detection in detections:
@@ -42,7 +49,8 @@ while True:
         # Calculate the position for the text (below the tag)
         text_x = int(min(detection.corners[:, 0]))
         text_y = int(max(detection.corners[:, 1])) + 20
-
+        
+        
         # Draw the tag_id text
         cv2.putText(frame, f"Tag ID: {detection.tag_id}", (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
 
